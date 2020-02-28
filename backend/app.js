d8264374410c39755a99078128f139a653a2797c
michaelsurya <shura.mike22@yahoo.com>
2020-02-28 11:59:12 +0000

    Fixed mongoDB deprecation warning

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const routes = require('./routes/routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const routes = require("./routes/routes");
 
 const app = express();
 
 mongoose.Promise = global.Promise;
-if (process.env.NODE_ENV !== 'test') {
-  mongoose.connect('mongodb://localhost/weeat');
-}
+// if (process.env.NODE_ENV !== "test") {
+  mongoose.connect("mongodb://localhost/weeat", { useNewUrlParser: true, useUnifiedTopology:true }) ;
+  mongoose.connection
+    .once("open", () => {
+      console.log("Connection succesful");
+    })
+    .on("error", error => {
+      console.warn("Warning", error);
+    });
+// }
 
 app.use(bodyParser.json());
 routes(app);
