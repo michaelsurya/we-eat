95485742f17b4ae569051275fba4740e7f2c4783
michaelsurya <shura.mike22@yahoo.com>
2020-04-01 01:42:29 +0100

    Added edit route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,11 +4,11 @@ import Footer from "./Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import PrivateRoute from "./auth/PrivateRoute";
 
+import EditProfile from "./profile/EditProfile";
 import Home from "./Home";
 import Profile from "./Profile";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
-
 
 const App = () => {
   return (
@@ -26,7 +26,12 @@ const DefaultContainer = () => (
   <div>
     <Nav></Nav>
     <Route path="/" exact component={Home} />
-    <Route path="/profile/:id" component={Profile} />
+    <Route path="/profile/:id" exact component={Profile} />
+    <PrivateRoute
+      path="/profile/edit/:id"
+      exact
+      component={EditProfile}
+    ></PrivateRoute>
     <Footer></Footer>
   </div>
 );
